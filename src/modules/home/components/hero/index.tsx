d8836c5de572fa3ddd8c0d1c78a253fb5579805e
Mahmoud Galal <ssss.aaa2002@gmail.com>
2023-06-16 04:53:57 +0300

    resize the hero, responsive

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -4,16 +4,16 @@ import Image from "next/image"
 const Hero = () => {
   return (
     <div className="h-[90vh] w-full relative">
-      <div className="text-white absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:text-left small:justify-end small:items-start small:p-32">
-        <h1 className="text-2xl-semi mb-4 drop-shadow-md shadow-black">
+      <div className="text-white absolute inset-0 z-10 max-sm:mt-[5rem] flex flex-col justify-center items-center text-center small:text-left small:justify-end small:items-start small:p-32">
+        <h1 className="text-2xl-semi max-sm:text-xl-semi mb-4 drop-shadow-md shadow-black">
           كل ما تحتاجه يخص التدخين الالكتروني
         </h1>
-        <p className="text-base-regular max-w-[32rem] mb-6 drop-shadow-md shadow-black">
+        <p className="text-base-regular max-w-[32rem] drop-shadow-md shadow-black">
           كل منتجات الشيشة الالكترونيه, الانيقه التي تناسب احتياجك 
         </p>
         <UnderlineLink href="/store">المتجر</UnderlineLink>
       </div>
-      <video autoPlay loop muted className="absolute inset-0 object-cover  h-1/2 xl:h-fit" src="/hero.mp4"/>
+      <video autoPlay loop muted className="absolute inset-0 object-cover h-1/2 max-sm:h-4/6 xl:h-fit" src="/hero.mp4"/>
       
     </div>
   )
